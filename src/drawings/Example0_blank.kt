31ca6f1a6c8988e2beabe3193843533c35219da3
Aravin <aravin@mentis.ca>
2019-05-09 20:19:17 -0400

    Add empty drawing example
    This serves as a template for creating new drawings quickly.

---
+++ src/drawings/Example0_blank.kt
@@ -0,0 +1,16 @@
+package drawings
+
+import koan.*
+import kotlin.math.*
+
+// Defines a drawing that is 500 by 500 pixels in size.
+class Example0_blank : Drawing(500, 500) {
+
+    // This function draws your drawing!
+    override fun draw() {
+
+        // Put your code here! Check the other examples to
+        // get an idea of things you can do.
+
+    }
+}
