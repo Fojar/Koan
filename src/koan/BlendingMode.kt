bf6d8f57fa64ed9d18a784d25ed0b7b96e68a3d8
Aravin <aravin@mentis.ca>
2019-05-25 17:23:43 -0400

    Simplify and optimize custom blending modes

--- src/koan/BlendingMode.kt
+++ src/koan/BlendingMode.kt
@@ -36,9 +36,9 @@ class CustomCompositeContext(val blendFunc: (IntArray, IntArray, IntArray) -> Un
 
     override fun dispose() {}
 
-    private val result = IntArray(3)
-    private val srcPixel = IntArray(3)
-    private val dstPixel = IntArray(3)
+    private val result = IntArray(4)
+    private val srcPixel = IntArray(4)
+    private val dstPixel = IntArray(4)
 
     override fun compose(src: Raster, dstIn: Raster, dstOut: WritableRaster) {
         val width = min(src.width, dstIn.width)
@@ -54,15 +54,14 @@ class CustomCompositeContext(val blendFunc: (IntArray, IntArray, IntArray) -> Un
 
                 srcPixels[x].splitRgbToArray(srcPixel)
                 dstPixels[x].splitRgbToArray(dstPixel)
-                val alpha = srcPixels[x] shr 24 and 0xFF
 
                 blendFunc(srcPixel, dstPixel, result)
 
                 // Mix the result into the destination using alpha.
                 dstPixels[x] = 255 shl 24 or (  // Destination alpha is always 255
-                        mixComponent(dstPixel[0], result[0], alpha) and 0xFF shl 16) or (
-                        mixComponent(dstPixel[1], result[1], alpha) and 0xFF shl 8) or (
-                        mixComponent(dstPixel[2], result[2], alpha) and 0xFF)
+                        result[1] and 0xFF shl 16) or (
+                        result[2] and 0xFF shl 8) or (
+                        result[3] and 0xFF)
             }
             dstOut.setDataElements(0, y, width, 1, dstPixels)
         }
@@ -71,24 +70,24 @@ class CustomCompositeContext(val blendFunc: (IntArray, IntArray, IntArray) -> Un
 
 }
 
-// Splits a pixel stored in ARGB form into separate RGB elements of the target array.
+// Splits a pixel stored in ARGB form into separate elements of the target array.
 private inline fun Int.splitRgbToArray(target: IntArray) {
-    target[0] = this shr 16 and 0xFF
-    target[1] = this shr 8 and 0xFF
-    target[2] = this and 0xFF
+    target[0] = this shr 24 and 0xFF
+    target[1] = this shr 16 and 0xFF
+    target[2] = this shr 8 and 0xFF
+    target[3] = this and 0xFF
 }
 
-private inline fun mixComponent(dst: Int, result: Int, alpha: Int) = dst + (result - dst) * alpha / 255
-
-
 private fun blendAdd(src: IntArray, dst: IntArray, result: IntArray) {
-    result[0] = min(255, src[0] + dst[0])
-    result[1] = min(255, src[1] + dst[1])
-    result[2] = min(255, src[2] + dst[2])
+    val alpha = src[0]
+    result[1] = min(255, src[1] * alpha / 255 + dst[1])
+    result[2] = min(255, src[2] * alpha / 255 + dst[2])
+    result[3] = min(255, src[3] * alpha / 255 + dst[3])
 }
 
 private fun blendSubtract(src: IntArray, dst: IntArray, result: IntArray) {
-    result[0] = max(0, dst[0] - src[0])
-    result[1] = max(0, dst[1] - src[1])
-    result[2] = max(0, dst[2] - src[2])
+    val alpha = src[0]
+    result[1] = max(0, dst[1] - src[1] * alpha / 255)
+    result[2] = max(0, dst[2] - src[2] * alpha / 255)
+    result[3] = max(0, dst[3] - src[3] * alpha / 255)
 }
