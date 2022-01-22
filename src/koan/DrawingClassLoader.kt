182c5c8e3fb4306d09d1eb79e7f99104b853c321
Aravin <aravin@mentis.ca>
2022-01-22 18:01:42 -0500

    Close InputStream promptly after use

--- src/koan/DrawingClassLoader.kt
+++ src/koan/DrawingClassLoader.kt
@@ -6,24 +6,24 @@ import java.io.File
 
 class DrawingClassLoader(parent: ClassLoader) : ClassLoader(parent) {
 
-    override fun loadClass(name: String): Class<*> {
+	override fun loadClass(name: String): Class<*> {
 
-        if (!name.startsWith("drawings.")) return super.loadClass(name)
+		if (!name.startsWith("drawings.")) return super.loadClass(name)
 
-        val b = getClassBytes(name)
-        return defineClass(name, b, 0, b.size)
-    }
+		val b = getClassBytes(name)
+		return defineClass(name, b, 0, b.size)
+	}
 
-    private fun getClassBytes(className: String): ByteArray {
+	private fun getClassBytes(className: String): ByteArray {
 
-        val inputStream = javaClass.classLoader.getResourceAsStream(
-            className.replace('.', File.separatorChar) + ".class"
-        )
+		return javaClass.classLoader.getResourceAsStream(
+			className.replace('.', File.separatorChar) + ".class"
+		)?.use {
+			val byteStream = ByteArrayOutputStream()
+			it.copyTo(byteStream)
+			byteStream.toByteArray()
+		} ?: throw Exception()
 
-        val byteStream = ByteArrayOutputStream()
-        inputStream.copyTo(byteStream)
-
-        return byteStream.toByteArray()
-    }
+	}
 
 }
